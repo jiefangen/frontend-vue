fa09919f5d79445be839a6ea887278eede4ce37b
JieFangen <jiefangen@gmail.com>
2023-08-18 12:21:55 +0800

    api文档以及监控页面外链跳转

--- admin-vue/src/views/monitor/druid/index.vue
+++ admin-vue/src/views/monitor/druid/index.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="iframe-container">
-    <iframe :src="iframeUrl" class="iframe" />
+  <div class="app-container">
+    <el-button type="success" plain @click="openNewTab">
+      <svg-icon icon-class="link" /> {{ $t('route.druidMonitor') }}
+    </el-button>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    const iframeUri = '/druid/index.html'
     return {
-      iframeUrl: process.env.VUE_APP_BASE_API + iframeUri
+    }
+  },
+  created() {
+    this.openNewTab()
+  },
+  methods: {
+    openNewTab() {
+      const hrefUrl = process.env.VUE_APP_BASE_API + '/druid/index.html'
+      // 在组件创建时自动跳转到目标页面
+      // window.location.href = hrefUrl
+      // 在组件创建时自动在新标签页中打开目标页面
+      window.open(hrefUrl, '_blank')
     }
   }
 }
 </script>
-
-<style scoped>
-  .iframe {
-    overflow-y: scroll;
-    border: none;
-    width: 100%;
-    height: 912px;
-  }
-</style>
---
+++ admin-vue/src/views/sample/embeddedPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="iframe-container">
+    <iframe :src="iframeUrl" class="iframe" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    const iframeUri = '/druid/index.html'
+    return {
+      iframeUrl: process.env.VUE_APP_BASE_API + iframeUri
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .iframe {
+    overflow-y: scroll;
+    border: none;
+    width: 100%;
+    height: 912px;
+  }
+</style>
