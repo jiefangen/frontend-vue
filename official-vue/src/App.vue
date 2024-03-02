8310426a8a2d8b0400780548f52abc742dc1adb9
JieFangen <jiefangen@gmail.com>
2024-03-02 18:09:48 +0800

    技术官网项目初始化提交

---
+++ official-vue/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useHead } from '@vueuse/head'
+
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'official-vue',
+  meta: [
+    { name: 'description', content: 'official-vue' }
+  ]
+})
+</script>
+
+<template>
+  <router-view />
+</template>
